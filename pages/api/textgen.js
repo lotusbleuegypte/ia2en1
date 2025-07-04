5c63f1ecf9203b4a30cf14ce3091a6e928884b98
lotusbleuegypte <lotusbleuegypte@gmail.com>
2025-07-04 13:46:03 +0200

    Update textgen.js

--- pages/api/textgen.js
+++ pages/api/textgen.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      model: "openai/gpt-3.5-turbo",
+      model: "mistralai/mistral-7b-instruct",
       messages: [{ role: "user", content: prompt }],
     }),
   });
